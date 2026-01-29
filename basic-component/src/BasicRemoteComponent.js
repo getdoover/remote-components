049191e17d4dae8bbfb5c141c2497c4b0b7e1e48
ThomasCrund <39323104+ThomasCrund@users.noreply.github.com>
2026-01-29 16:55:04 +1100

    Removed get agent call from remote component

--- basic-component/src/BasicRemoteComponent.js
+++ basic-component/src/BasicRemoteComponent.js
@@ -1,17 +1,9 @@
 import RemoteAccess from 'customer_site/RemoteAccess';
 import RemoteComponentWrapper from 'customer_site/RemoteComponentWrapper';
-import {CircularProgress, Grid} from '@mui/material';
-import {useRemoteParams} from "customer_site/useRemoteParams";
-import {useAgent} from "customer_site/hooks";
+
 class BasicRemoteComponentInner extends RemoteAccess {
   constructor(props) {
     super(props);
-    this.state = {
-      agent: null,
-    };
-
-    this.agent = props.agent;
-
   }
 
   render() {
@@ -29,23 +21,11 @@ class BasicRemoteComponentInner extends RemoteAccess {
 
 // ok yeah this is a bit dumb but we want to be able to use the hooks to get agent
 // and to do that we need the provider context set...
-const ComponentWithAgent = (props) => {
-  const {agentId} = useRemoteParams();
-  const {agent} = useAgent(agentId);
-
-  if (!agent) {
-    return <CircularProgress/>;
-  }
-
-  return (
-    <BasicRemoteComponentInner agent={agent} {...props}/>
-  );
-}
 
 const BasicRemoteComponent = (props) => {
   return (
     <RemoteComponentWrapper>
-      <ComponentWithAgent {...props}/>
+      <BasicRemoteComponentInner {...props}/>
     </RemoteComponentWrapper>
   )
 }
